415c83b7b5da013af8f75b3b7662c2a306e97122
Administrator <Administrator@192.168.12.61>
2019-11-11 10:24:00 +0100

    ProgettoLoginRegistrazione

---
+++ ProgettoUtente/src/com/ats/utility/ConnectionFactory.java
@@ -0,0 +1,53 @@
+package com.ats.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	String driver="oracle.jdbc.driver.OracleDriver";
+	String connectionUrl="jdbc:oracle:thin:@localhost:1521:xe";
+	String dbUser="studente";
+	String dbPassword="0000";
+
+
+	private static ConnectionFactory connectionFactory=null;
+
+
+	private ConnectionFactory(){
+
+		try {
+			//reflection
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+	public Connection getConnection(){
+		Connection con=null;
+		try {
+			con= DriverManager.getConnection(connectionUrl,dbUser,dbPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	//get instance istanzia automaticamente l'oggetto connectionFactory richiamando 
+	// il costruttore
+	// pattern singleton
+	public static ConnectionFactory getIstance(){
+
+		if(connectionFactory==null){
+			connectionFactory= new ConnectionFactory();
+		}
+
+		return connectionFactory;
+	}
+
+}
